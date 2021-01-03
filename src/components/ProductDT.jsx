04318ac211d7da08b7d67cf590742727eaaecad3
fatihgvnc <heatclife007@gmail.com>
2021-01-03 17:59:49 +0300

    1 file modified

--- src/components/ProductDT.jsx
+++ src/components/ProductDT.jsx
@@ -5,12 +5,14 @@ import { products } from "../data/products";
 export default function ProductDT() {
   const { productID } = useParams();
 
-  const product = products.filter(item => item._id === Number(productID));
-  return (
-    <div className="mt-5">
-      <h1 className="mb-5">Product:</h1>
-      <h2>{product[0].title}</h2>
-      <p>{product[0].detail}</p>
-    </div>
-  );
+  const product = products
+    .filter(item => item._id === Number(productID))
+    .map(item => (
+      <div className="mt-5">
+        <h1 className="mb-5">Product:</h1>
+        <h2>{item.title}</h2>
+        <p>{item.detail}</p>
+      </div>
+    ));
+  return <>{product}</>;
 }
